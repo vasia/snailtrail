887a4930f46548b46881fc5caac38ced6494742d
Malte Sandstede <malte@sandstede.com>
2019-04-25 12:07:50 +0200

    update tcp logger example

--- timely-adapter/examples/tcp_logger.rs
+++ timely-adapter/examples/tcp_logger.rs
@@ -1,15 +1,20 @@
 use std::time::Duration;
 
+use timely::logging::ScheduleEvent;
+use timely::logging::OperatesEvent;
+use timely::logging::TimelyEvent::Operates;
+use timely::logging::TimelyEvent::Schedule;
 use timely_adapter::record_collection;
 use timely_adapter::connect::{open_sockets, make_replayers};
 use timely_adapter::connect::{make_file_replayers};
 use timely::dataflow::operators::capture::replay::Replay;
 use timely::dataflow::operators::probe::Probe;
+use timely::dataflow::InputHandle;
+use timely::logging::{StartStop, TimelyEvent};
 
 fn main() {
     // the number of workers in the computation we're examining
     let source_peers = std::env::args().nth(1).unwrap().parse::<usize>().unwrap();
-    let iterations = std::env::args().nth(2).unwrap().parse::<u64>().unwrap();
 
     // one socket per worker in the computation we're examining
     // let sockets = open_sockets(source_peers);
@@ -28,9 +33,7 @@ fn main() {
             // let stream = replayers.replay_into(scope);
             let stream = record_collection(scope, replayers);
 
-            stream
-                // .inspect(|x| println!("@{:?} : {}", x.1, x.0))
-                .probe()
+            stream.probe()
         });
 
         // let mut timer = std::time::Instant::now();
@@ -38,24 +41,8 @@ fn main() {
         while !probe.done() {
             worker.step();
         }
-        println!("done: {}s", timer.elapsed().as_secs());
-        // while epoch < Duration::new(iterations,0) {
-        //     while probe.less_equal(&epoch) {
-        //         worker.step();
-        //     }
-        //     timer = std::time::Instant::now();
-        //     epoch += Duration::new(0, 10000000);
-        // }
 
-        // let mut epoch = Duration::new(0,0);
-        // while epoch < Duration::new(10, 0) {
-        //     let timer = std::time::Instant::now();
-        //     while probe.less_equal(&epoch) {
-        //         worker.step();
-        //     }
-        //     println!("{}@{:?}: {}", idx, epoch, timer.elapsed().as_millis());
-        //     epoch += Duration::new(1,0);
-        // }
+        println!("done: {}s", timer.elapsed().as_secs());
 
         // stall application
         // use std::io::stdin;
