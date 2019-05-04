ce7e93f1e916eb0b32a19c61687d96d4afc24895
Malte Sandstede <malte@sandstede.com>
2019-05-04 18:53:38 +0200

    - use epoch timestamp in pag - handle negative diffs in pag construction

--- timely-snailtrail/src/pag.rs
+++ timely-snailtrail/src/pag.rs
@@ -80,7 +80,7 @@ pub fn create_pag<S: Scope<Timestamp = Duration>, R: 'static + Read>(
     // let data_edges = make_data_edges(&records);
 
     local_edges
-    // .concat(&control_edges)
+        .concat(&control_edges)
     // .concat(&data_edges)
 }
 
@@ -94,66 +94,62 @@ fn make_local_edges<S: Scope<Timestamp = Duration>>(
             let mut prev_node: Option<PagNode> = None;
 
             for (record, diff) in input {
-                if *diff > 0 {
-                    if prev_record.is_none() {
-                        // first node for this worker
-                        prev_node = Some(PagNode::from(*record));
-                        prev_record = Some(*record);
-                    } else {
-                        let make_edge_type = |prev: &LogRecord, curr: &LogRecord| {
-                            match (prev.event_type, curr.event_type) {
-                                // SchedStart ----> SchedEnd
-                                (EventType::Start, EventType::End) => ActivityType::Scheduling,
-                                // SchedEnd ----> SchedStart
-                                (EventType::End, EventType::Start) => ActivityType::BusyWaiting,
-                                // something ---> msgreceive
-                                (_, EventType::Received) => ActivityType::Waiting,
-                                // schedend -> remotesend, remote -> schedstart, remote -> remotesend
-                                (_, _) => ActivityType::Unknown, // @TODO
-                            }
-                        };
-
-                        let make_op_id = |prev: &LogRecord, curr: &LogRecord| {
-                            if prev.event_type == EventType::Start
-                                && curr.event_type == EventType::End
-                            {
-                                curr.operator_id
-                            } else {
-                                None
-                            }
-                        };
-
-                        let destination = PagNode::from(*record);
-
-                        let edge_type = make_edge_type(&prev_record.unwrap(), &record);
-                        let edge = PagEdge {
-                            source: prev_node.unwrap(),
-                            destination,
-                            edge_type,
-                            operator_id: make_op_id(&prev_record.unwrap(), &record),
-                            traverse: if edge_type == ActivityType::Waiting {
-                                TraversalType::Block
-                            } else {
-                                TraversalType::Unbounded
-                            },
-                        };
-
-                        output.push((edge, 1));
-
-                        prev_node = Some(destination);
-                        prev_record = Some(*record);
-                    }
+                if prev_record.is_none() {
+                    // first node for this worker
+                    prev_node = Some(PagNode::from(*record));
+                    prev_record = Some(*record);
                 } else {
-                    unimplemented!();
+                    let make_edge_type = |prev: &LogRecord, curr: &LogRecord| {
+                        match (prev.event_type, curr.event_type) {
+                            // SchedStart ----> SchedEnd
+                            (EventType::Start, EventType::End) => ActivityType::Scheduling,
+                            // SchedEnd ----> SchedStart
+                            (EventType::End, EventType::Start) => ActivityType::BusyWaiting,
+                            // something ---> msgreceive
+                            (_, EventType::Received) => ActivityType::Waiting,
+                            // schedend -> remotesend, remote -> schedstart, remote -> remotesend
+                            (_, _) => ActivityType::Unknown, // @TODO
+                        }
+                    };
+
+                    let make_op_id = |prev: &LogRecord, curr: &LogRecord| {
+                        if prev.event_type == EventType::Start
+                            && curr.event_type == EventType::End
+                        {
+                            curr.operator_id
+                        } else {
+                            None
+                        }
+                    };
+
+                    let destination = PagNode::from(*record);
+
+                    let edge_type = make_edge_type(&prev_record.unwrap(), &record);
+                    let edge = PagEdge {
+                        source: prev_node.unwrap(),
+                        destination,
+                        edge_type,
+                        operator_id: make_op_id(&prev_record.unwrap(), &record),
+                        traverse: if edge_type == ActivityType::Waiting {
+                            TraversalType::Block
+                        } else {
+                            TraversalType::Unbounded
+                        },
+                    };
+
+                    output.push((edge, *diff));
+
+                    prev_node = Some(destination);
+                    prev_record = Some(*record);
                 }
             }
         })
-        .map(|(key, x)| x)
+        .map(|(_key, x)| x)
 }
 
 fn make_control_edges<S: Scope<Timestamp = Duration>>(
     records: &Collection<S, LogRecord, isize>,
-) -> Collection<S, (u64, PagEdge), isize> {
+) -> Collection<S, PagEdge, isize> {
     let control_messages_send = records
         .filter(|x| {
             x.activity_type == ActivityType::ControlMessage && x.event_type == EventType::Sent
@@ -161,32 +157,17 @@ fn make_control_edges<S: Scope<Timestamp = Duration>>(
         .map(|x| ((x.local_worker, x.correlator_id, x.channel_id), x));
 
     let control_messages_received = records
-        .filter(|x| {
-            x.activity_type == ActivityType::ControlMessage && x.event_type == EventType::Received
-        })
-        .map(|x| {
-            (
-                (
-                    x.remote_worker.unwrap(),
-                    x.correlator_id,
-                    x.channel_id,
-                ),
-                x,
-            )
-        });
+        .filter(|x| {x.activity_type == ActivityType::ControlMessage && x.event_type == EventType::Received})
+        .map(|x| {((x.remote_worker.unwrap(), x.correlator_id, x.channel_id,), x,)});
 
     control_messages_send
         .join(&control_messages_received)
-        .map(|(key, (from, to))| {
-            (
-                key.0,
-                PagEdge {
-                    source: PagNode::from(&from),
-                    destination: PagNode::from(&to),
-                    edge_type: ActivityType::ControlMessage,
-                    operator_id: None,
-                    traverse: TraversalType::Unbounded,
-                },
-            )
-        })
+        .map(|(_key, (from, to))|
+             PagEdge {
+                 source: PagNode::from(&from),
+                 destination: PagNode::from(&to),
+                 edge_type: ActivityType::ControlMessage,
+                 operator_id: None,
+                 traverse: TraversalType::Unbounded,
+             })
 }
