50484f1906ef31f313aeb9968c0aa51310f566d4
Malte Sandstede <malte@sandstede.com>
2019-05-04 18:38:05 +0200

    refine batch-based log_pag

--- timely-adapter/src/connect.rs
+++ timely-adapter/src/connect.rs
@@ -30,6 +30,9 @@ use timely::{
 use TimelyEvent::{Operates, Channels, Progress, Messages, Schedule, Text};
 
 
+// @TODO: pass as CLA
+const MAX_FUEL: isize = 1;
+
 /// Listens on 127.0.0.1:8000 and opens `source_peers` sockets from the
 /// computations we're examining (one socket for every worker on the
 /// examined computation)
@@ -89,30 +92,14 @@ pub fn make_file_replayers(
         .collect::<Vec<_>>()
 }
 
-/// Logger to use for computation event logging
-pub enum Logger {
-    /// Batches events to TCP or file. Outputs each batch at last event's
-    /// time. No further event preprocessing in any way.
-    All,
-    /// Batches events to TCP or file. Filters events so that they are
-    /// optimized for use in PAG construction.
-    Pag,
-}
-
-// @TODO: pass as CLA
-const MAX_FUEL: isize = 30_000;
-
 /// Batched logging of events to TCP or file.
 /// For live analysis, provide `SNAILTRAIL_ADDR` as env variable.
 /// Else, the computation will log to file for later replay.
-pub fn register_logger(worker: &mut Worker<Generic>, logger: Logger) {
+pub fn register_logger(worker: &mut Worker<Generic>) {
     if let Ok(addr) = ::std::env::var("SNAILTRAIL_ADDR") {
         if let Ok(stream) = TcpStream::connect(&addr) {
             let writer = EventWriter::new(stream);
-            match logger {
-                Logger::All => log_all(worker, writer),
-                Logger::Pag => log_pag(worker, writer),
-            };
+            log_pag(worker, writer);
         } else {
             panic!("Could not connect logging stream to: {:?}", addr);
         }
@@ -154,63 +141,111 @@ fn log_all<W: 'static + Write> (worker: &mut Worker<Generic>, writer: EventWrite
         });
 }
 
-// @TODO docstring
-fn log_pag<W: 'static + Write> (worker: &mut Worker<Generic>, mut writer: EventWriter<Duration, (Duration, usize, TimelyEvent), W>) {
+/// Registers a `TimelyEvent` logger which outputs relevant log events for PAG construction.
+/// 1. Only relevant events are written to `writer`.
+/// 2. Using `Text` events as markers, logged events are written out at one time per epoch.
+/// 3. For higher performance, multiple event batches are written out together as a batch.
+/// 4. Dataflow setup is collapsed into t=1ns so that peel_operators is more efficient.
+/// 5. If the computation is bounded, capabilities will be dropped correctly at the end of
+///    computation.
+fn log_pag2<W: 'static + Write> (worker: &mut Worker<Generic>, mut writer: EventWriter<Duration, (Duration, usize, TimelyEvent), W>) {
     use std::collections::HashMap;
 
+    // every `fuel` events, they are written out to `writer`
     let mut fuel = MAX_FUEL;
+
+    // buffer of epochs and events for that epoch
     let mut buffer = HashMap::new();
+
+    // epoch times in the order they advance (used to write out
+    // `buffer` in correct order
     let mut time_order = Vec::new();
 
+    // first real frontier, used for setting up the computation
+    // (`Operates` et al.)
+    let mut frontier = Duration::from_nanos(1);
+    time_order.push(frontier);
+
+    // initialized to 0, which we'll drop as soon as the
+    // computation makes progress
+    let mut old_frontier = Duration::default();
+
+    // keeps track of whether the computation is shutting down to
+    // free up last frontier and write out remaining log messages.
+    let mut done = false;
+
+    // debug logs
     let index = worker.index();
-
-    let mut frontier = Duration::new(0,1);
-    time_order.push(frontier.clone());
-
-    let mut old_frontier = Duration::new(0,0);
+    let mut total = 0;
 
     worker.log_register()
         .insert::<TimelyEvent,_>("timely", move |time, data| {
-
-            if data.len() > 0 {
-                fuel -= 1;
-            }
-
             for tuple in data.drain(..) {
                 match &tuple.2 {
-                    Operates(_) | Channels(_) | Progress(_) | Messages(_) | Schedule(_)  => {
+                    Channels(_) | Progress(_) | Messages(_) | Schedule(_)  => {
+                        fuel -= 1;
                         let entry = buffer.entry(frontier).or_insert(Vec::new());
                         entry.push(tuple);
                     },
-                    Text(_) => {
+                    Operates(_) => {
+                        fuel -= 1;
                         let entry = buffer.entry(frontier).or_insert(Vec::new());
-                        entry.push(tuple);
-                        frontier = time.clone();
-                        time_order.push(frontier.clone());
+
+                        // all operates events should happen in the initialization epoch
+                        assert!(frontier == Duration::from_nanos(1));
+                        entry.push((frontier, tuple.1, tuple.2));
+                    },
+                    // Text events mark epochs in the computation
+                    Text(e) => {
+                        if e.starts_with("[st] computation done") {done = true;}
+
+                        fuel -= 1;
+
+                        // advance frontier at which events are buffered to current time
+                        frontier = *time;
+                        time_order.push(frontier);
                     },
                     _ => {}
                 }
             }
 
-            // every `fuel` event batches, we push them all to the writer
-            if fuel <= 0 {
+            if fuel <= 0 || done {
                 fuel = MAX_FUEL;
-                // println!("pag publishing {}@{:?}", index, &time);
-
-                // the newest frontier is wip, so we won't consider it
-                let newest_frontier = time_order.pop().expect("time_order should never be empty");
+                // the newest frontier is still WIP, so don't consider it
+                let newest_frontier = time_order.pop();
 
                 // we iterate through the time_order to make sure we push events in the correct order
-                for time in time_order.drain(..) {
-                    let batch = buffer.remove(&time).expect("time_order and buffer should stay in sync");
+                for mut time in time_order.drain(..) {
+                    if let Some(batch) = buffer.remove(&time) {
+                        // debug logging
+                        total += batch.len();
+                        info!("sent epoch {:?}@w{}, count: {}, total: {}", time, index, batch.len(), total);
 
-                    // println!("publish @{:?}, count: {}", time, batch.len());
+                        writer.push(Event::Messages(time, batch));
 
-                    writer.push(Event::Messages(time, batch));
-                    writer.push(Event::Progress(vec![(time, 1), (old_frontier, -1)]));
-                    old_frontier = time;
+                        time += Duration::from_nanos(1); // frontier should move past the current batch
+                        writer.push(Event::Progress(vec![(time, 1), (old_frontier, -1)]));
+
+                        old_frontier = time;
+                    } else {
+                        assert!(done == true, "time order & buffer should only desync on logging wrap-up");
+                    }
                 }
-                time_order.push(newest_frontier.clone());
+
+                // add the newest frontier back in
+                if let Some(frontier) = newest_frontier {
+                    time_order.push(frontier);
+                } else {
+                    assert!(done == true, "time order should only dry up on logging wrap-up");
+                }
+
+                if done {
+                    // prevents this from being called over and over
+                    done = false;
+
+                    info!("timely logging wrapping up");
+                    writer.push(Event::Progress(vec![(old_frontier, -1)]));
+                }
             }
         });
 }
