a3690a4cdfaa86e551ecea0e2d8c0ef2560ae36f
Malte <malte@sandstede.com>
2019-08-01 17:09:58 +0200

    update viz

--- timely-snailtrail/examples/viz.rs
+++ timely-snailtrail/examples/viz.rs
@@ -1,12 +1,10 @@
-use differential_dataflow::lattice::Lattice;
-
-use timely::dataflow::Scope;
-use timely::dataflow::operators::{capture::{event::Event, Capture, replay::Replay, extract::Extract}};
+use timely::dataflow::operators::{capture::{event::Event, Capture, extract::Extract}};
 use timely::dataflow::operators::map::Map;
 use timely::dataflow::operators::capture::EventReader;
 use timely::communication::initialize::WorkerGuards;
+use timely::dataflow::operators::filter::Filter;
 
-use timely_snailtrail::{pag, Config};
+use timely_snailtrail::pag;
 use timely_snailtrail::pag::PagEdge;
 
 use logformat::pair::Pair;
@@ -22,26 +20,9 @@ static HTML: &str = include_str!("../html/index.html");
 use tdiag_connect::receive as connect;
 use tdiag_connect::receive::ReplaySource;
 
+
 fn main() {
-    let mut args = std::env::args();
-    let _ = args.next().unwrap();
-    let worker_peers = args.next().expect("no worker peers").parse::<usize>().expect("invalid worker peers");
-    let source_peers = args.next().expect("no source peers").parse::<usize>().expect("invalid source peers");
-    let output_path = args.next().expect("missing output path");
-    let from_file = if let Some(_) = args.next() {
-        true
-    } else {
-        false
-    };
-
-    let config = Config {
-        timely_args: vec!["-w".to_string(), worker_peers.to_string()],
-        worker_peers,
-        source_peers,
-        from_file,
-    };
-
-    let (worker_handles, pag) = prep_pag(config);
+    let (output_path, worker_handles, pag) = prep_pag();
 
     {
         use std::io;
@@ -50,8 +31,6 @@ fn main() {
         let mut stdin = io::stdin();
         let mut stdout = io::stdout();
 
-        // write!(stdout, "Press enter to generate graph (this will crash the source computation if it hasn't terminated).")
-        //     .expect("failed to write to stdout");
         write!(stdout, "Wait until computation has finished, then press any key to generate graph.")
             .expect("failed to write to stdout");
         stdout.flush().unwrap();
@@ -66,24 +45,41 @@ fn main() {
     println!("Graph generated in file://{}", std::fs::canonicalize(output_path).expect("invalid path").to_string_lossy());
 }
 
-fn prep_pag(config: Config) -> (WorkerGuards<()>, mpsc::Receiver<Event<Pair<u64, Duration>, PagEdge>>) {
+fn prep_pag() -> (String, WorkerGuards<()>, mpsc::Receiver<Event<Pair<u64, Duration>, PagEdge>>) {
+    let mut args = std::env::args();
+
+    let _ = args.next(); // bin name
+    let source_peers = args.next().unwrap().parse::<usize>().unwrap();
+    let output_path = args.next().expect("missing output path");
+    let online = if args.next().unwrap().parse::<bool>().unwrap() {
+        Some((args.next().unwrap().parse::<String>().unwrap(), std::env::args().nth(5).unwrap().parse::<u16>().unwrap()))
+    } else {
+        None
+    };
+    let _ = args.next(); // --
+
+    let args = args.collect::<Vec<_>>();
+
     // creates one socket per worker in the computation we're examining
-    let replay_source = if config.from_file {
-        let files = (0 .. config.source_peers)
+    let replay_source = if let Some((ip, port)) = &online {
+        let sockets = connect::open_sockets(ip.parse().expect("couldn't parse IP"), *port, source_peers).expect("couldn't open sockets");
+        ReplaySource::Tcp(Arc::new(Mutex::new(sockets)))
+    } else {
+        let files = (0 .. source_peers)
             .map(|idx| format!("{}.dump", idx))
             .map(|path| Some(PathBuf::from(path)))
             .collect::<Vec<_>>();
 
         ReplaySource::Files(Arc::new(Mutex::new(files)))
-    } else {
-        let sockets = connect::open_sockets("127.0.0.1".parse().expect("couldn't parse IP"), 8000, config.source_peers).expect("couldn't open sockets");
-        ReplaySource::Tcp(Arc::new(Mutex::new(sockets)))
     };
 
     let (pag_send, pag_recv) = mpsc::channel();
     let pag_send = Arc::new(Mutex::new(pag_send));
 
-    let worker_handles = timely::execute_from_args(config.timely_args.clone().into_iter(), move |worker| {
+
+    let worker_handles = timely::execute_from_args(args.clone().into_iter(), move |worker| {
+        let index = worker.index();
+
         let pag_send: mpsc::Sender<_> = pag_send.lock().expect("cannot lock pag_send").clone();
 
         // read replayers from file (offline) or TCP stream (online)
@@ -91,15 +87,15 @@ fn prep_pag(config: Config) -> (WorkerGuards<()>, mpsc::Receiver<Event<Pair<u64,
             connect::make_readers(replay_source.clone(), worker.index(), worker.peers()).expect("couldn't create readers");
 
         worker.dataflow(|scope| {
-            pag::create_pag(scope, readers, 0)
-                .inner
+            pag::create_pag(scope, readers, index, 1)
                 .map(|(x, _t, _diff)| x)
+                .filter(|x| x.source.epoch == 2)
                 .capture_into(pag_send);
         });
     })
     .expect("timely error");
 
-    (worker_handles, pag_recv)
+    (output_path, worker_handles, pag_recv)
 }
 
 pub fn render(output_path: &String, worker_handles: WorkerGuards<()>, pag: mpsc::Receiver<Event<Pair<u64, Duration>, PagEdge>>) {
@@ -113,10 +109,16 @@ pub fn render(output_path: &String, worker_handles: WorkerGuards<()>, pag: mpsc:
     for edge in pag.extract().into_iter().flat_map(|(_t, v)| v) {
         expect_write(writeln!(
             file,
-            "{{ w: {}, t: [{}, {}] }},",
-            edge.source.worker_id,
+            "{{ src: {{ t: {}, w: {}, e: {} }}, dst: {{ t: {}, w: {}, e: {} }}, type: \"{:?}\", o: \"{:?}\", tr: \"{:?}\" }},",
             edge.source.timestamp.as_nanos(),
-            edge.destination.timestamp.as_nanos()
+            edge.source.worker_id,
+            edge.source.epoch,
+            edge.destination.timestamp.as_nanos(),
+            edge.destination.worker_id,
+            edge.destination.epoch,
+            edge.edge_type,
+            edge.operator_id,
+            edge.traverse,
         ))
     }
     expect_write(writeln!(file, "];"));
